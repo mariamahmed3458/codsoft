13fc0a43a91bc3c956e038d98ffccedf6ac1f13a
mariamahmed3458 <146301143+mariamahmed3458@users.noreply.github.com>
2023-09-28 13:59:59 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -22,3 +22,11 @@ int main() {
     cout << "Correct!\n";
     return 0;
 }
+// Task 1 : Number guessing game 
+//First in this code i included cstdlib library to use rand function .
+//In main function, first i declared two integer variables , one for rand and  i have put 
+//limit to rand function to prevent any error could happen (like randomly choose very large number) 
+//and the other for the guess by the user and then the game asks the user to enter their guess and 
+//the game provides a feedback on whether the guess is higher or lower than the number and it keeps looping until the user guess it correctly. 
+
+// In this task , I have developed my skills in learning new libraries and functions .
